57965f739f87bd44ff9cd657030df3d0e47cb383
allanlcomander <andala63@gmail.com>
2021-01-18 10:10:26 +0300

    finish editing mobile views

--- js/dist/scripts.dev.js
+++ js/dist/scripts.dev.js
@@ -1,11 +1,5 @@
 "use strict";
 
-// var toggleButton = document.querySelector("#button");
-// toggleButton.addEventListener('click', function() {
-//     // Then toggle (add/remove) anually class
-//     document.querySelector("p").classList.toggle('annually'); 
-//     document.querySelector("p").classList.toggle('monthly');
-//   });
 $(document).ready(function () {
   $("#button").click(function () {
     $(".annually").toggle();
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,4 @@
-// var toggleButton = document.querySelector("#button");
-// toggleButton.addEventListener('click', function() {
-//     // Then toggle (add/remove) anually class
-//     document.querySelector("p").classList.toggle('annually'); 
-//     document.querySelector("p").classList.toggle('monthly');
-//   });
+
 
 $(document).ready(function(){
     $("#button").click(function(){

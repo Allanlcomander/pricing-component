d84ff11adbbe9d5baedc84d207b012e924d51be1
allanlcomander <andala63@gmail.com>
2021-01-18 08:19:44 +0300

    edit toggle button and functionality

--- js/dist/scripts.dev.js
+++ js/dist/scripts.dev.js
@@ -1,13 +1,15 @@
+"use strict";
+
 // var toggleButton = document.querySelector("#button");
 // toggleButton.addEventListener('click', function() {
 //     // Then toggle (add/remove) anually class
 //     document.querySelector("p").classList.toggle('annually'); 
 //     document.querySelector("p").classList.toggle('monthly');
 //   });
-
-$(document).ready(function(){
-    $("#button").click(function(){
-        $(".annually").toggle();
-        $(".monthly").toggle();
-    });
+$(document).ready(function () {
+  $("#button").click(function () {
+    $(".annually").toggle();
+    $(".monthly").toggle();
+  });
 });
+//# sourceMappingURL=scripts.dev.js.map
